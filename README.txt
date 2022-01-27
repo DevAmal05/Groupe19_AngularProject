a579462015208234010a6730a4c160c6ba239d30
DevAmal05 <62183621+DevAmal05@users.noreply.github.com>
2022-01-27 21:58:56 +0200

    init

--- README.txt
+++ README.txt
@@ -12,4 +12,12 @@ Create
        cd helloworld
        code .
        ng serve 
+
+
+Component
+       ng g c name_component
+
+Bootstrap
+       npm install bootstrap
+       "node_modules/bootstrap/dist/css/bootstrap.min.css" (angular.json)
        
--- helloworld/src/app/app-routing.module.ts
+++ helloworld/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'test',component:TestComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
